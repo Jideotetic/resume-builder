1c8144197bf9b634f162a751b97de74a69b03cbf
Jideotetic <jideotetic@gmail.com>
2023-08-26 15:32:45 +0100

    Added the footer section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import Favicon from './Favicon';
 import Header from './Header';
+import Footer from './Footer';
 import '../styles/App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Favicon />
       <Header />
+      <Footer />
     </>
   );
 }
