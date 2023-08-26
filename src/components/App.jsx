e6f34860a2758c1604cd60402a7a426c247ea90c
Jideotetic <jideotetic@gmail.com>
2023-08-26 14:53:11 +0100

    Added the header section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import Favicon from './Favicon';
+import Header from './Header';
 import '../styles/App.css';
 
 function App() {
   return (
     <>
       <Favicon />
-      <h1>Resume Builder</h1>
+      <Header />
     </>
   );
 }
